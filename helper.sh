1e1e9169ac48707518d1dacab6cd747bb2eff7f9
Jason Miller <jake.admin@gmail.com>
2020-12-30 23:56:40 -0500

    fix error reporting

--- helper.sh
+++ helper.sh
@@ -5,27 +5,29 @@ fn_error () {
 }
 
 fn_dircheck () {
-    [[ ! -d /usr/local/bin/ ]] && mkdir -p /usr/local/bin || fn_error 'mkdir bin'
-    [[ ! -d /usr/local/lib/ ]] && mkdir -p /usr/local/lib || fn_error 'mkdir lib'
-    [[ ! -d /usr/local/src/ ]] && mkdir -p /usr/local/src || fn_error 'mkdir src'
+    [[ ! -d /usr/bin/ ]] && mkdir -p /usr/bin || fn_error 'mkdir bin'
+    [[ ! -d /usr/lib/ ]] && mkdir -p /usr/lib || fn_error 'mkdir lib'
+    [[ ! -d /usr/src/ ]] && mkdir -p /usr/src || fn_error 'mkdir src'
+    [[ ! -d /etc/cron.d/ ]] && mkdir -p /etc/cron.d || fn_error 'mkdir cron.d'
+
 }
 
 fn_cron () {
-    cp ./cronjob /usr/local/bin/cronjob || fn_error 'cp cronjob'
-    chmod 0744 /usr/local/bin/cronjob || fn_error 'chmod cronjob'
+    cp ./cronjob /usr/bin/cronjob || fn_error 'cp cronjob'
+    chmod 0744 /usr/bin/cronjob || fn_error 'chmod cronjob'
     cp ./crontab /etc/cron.d/weekly-phpupdate || fn_error 'cp crontab'
     chmod 0644 /etc/cron.d/weekly-phpupdate || fn_error 'chmod crontab'
     touch /var/log/cron.log || fn_error 'touch cron.log'
 }
 
 fn_phpenmod () {
-    cp ./phpenmod /usr/local/bin/phpenmod || fn_error 'cp phpenmod'
-    chmod 0755 /usr/local/bin/phpenmod || fn_error 'chmod phpenmod'
+    cp ./phpenmod /usr/bin/phpenmod || fn_error 'cp phpenmod'
+    chmod 0755 /usr/bin/phpenmod || fn_error 'chmod phpenmod'
 }
 
 fn_phpserv () {
-    cp ./phpserv /usr/local/bin/phpserv || fn_error 'cp phpserv'
-    chmod 0755 /usr/local/bin/phpserv || fn_error 'chmod phpserv'
+    cp ./phpserv /usr/bin/phpserv || fn_error 'cp phpserv'
+    chmod 0755 /usr/bin/phpserv || fn_error 'chmod phpserv'
 }
 
 fn_dircheck;
