95b5629b40b078406a7739ea852e47712fc3a2fa
Jason Miller <jake.admin@gmail.com>
2020-12-31 00:20:25 -0500

    a

--- helper.sh
+++ helper.sh
@@ -12,20 +12,20 @@ fn_dircheck () {
 }
 
 fn_cron () {
-    [[ -e cronjob ]] && echo 'cronjob okay'; cp cronjob /usr/local/bin/cronjob || fn_error 'cp cronjob'
+    [[ -e /root/setup/cronjob ]] && echo 'cronjob okay'; cp /root/setup/cronjob /usr/local/bin/cronjob || fn_error 'cp cronjob'
     chmod 0744 /usr/local/bin/cronjob || fn_error 'chmod cronjob'
-    [[ -e crontab ]] && echo 'crontab okay'; cp ./crontab /etc/cron.d/weekly-phpupdate || fn_error 'cp crontab'
+    [[ -e /root/setup/crontab ]] && echo 'crontab okay'; cp /root/setup/crontab /etc/cron.d/weekly-phpupdate || fn_error 'cp crontab'
     chmod 0644 /etc/cron.d/weekly-phpupdate || fn_error 'chmod crontab'
     touch /var/log/cron.log || fn_error 'touch cron.log'
 }
 
 fn_phpenmod () {
-    [[ -e phpenmod ]] && echo 'phpenmod'; cp ./phpenmod /usr/local/bin/phpenmod || fn_error 'cp phpenmod'
+    [[ -e /root/setup/phpenmod ]] && echo 'phpenmod'; cp /root/setup/phpenmod /usr/local/bin/phpenmod || fn_error 'cp phpenmod'
     chmod 0755 /usr/local/bin/phpenmod || fn_error 'chmod phpenmod'
 }
 
 fn_phpserv () {
-    [[ -e phpserv ]] && echo 'phpserv okay'; cp ./phpserv /usr/local/bin/phpserv || fn_error 'cp phpserv'
+    [[ -e /root/setup/phpserv ]] && echo 'phpserv okay'; cp /root/setup/phpserv /usr/local/bin/phpserv || fn_error 'cp phpserv'
     chmod 0755 /usr/local/bin/phpserv || fn_error 'chmod phpserv'
 }
 
